8a47f2d38c6a0b1e5548733e78436ef94caa22ac
Simran <simu170715@gmail.com>
2020-12-10 10:00:16 +0530

    Authentication

--- app/src/main/java/com/example/patientconsulationapp/classes/Backend.kt
+++ app/src/main/java/com/example/patientconsulationapp/classes/Backend.kt
@@ -1,9 +1,34 @@
 package com.example.patientconsulationapp.classes
 
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.patientconsulationapp.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class Backend
 {
     // Create a instance of Firebase Firestore
-    private val mFireStore = FirebaseFirestore.getInstance()
+    private val mFireStore = Firebase.firestore.collection("User")
+
+    fun registerUser(user: User){
+        GlobalScope.launch(Dispatchers.IO)
+        {
+            mFireStore.document(getCurrentUserID()).set(user).await()
+        }
+
+
+    }
+    fun getCurrentUserID(): String {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        var currentUserId = ""
+        if (currentUser != null) {
+            currentUserId = currentUser.uid
+        }
+        return currentUserId
+
+    }
 }
--- app/src/main/java/com/example/patientconsulationapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/patientconsulationapp/ui/HomeFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import com.example.patientconsulationapp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
@@ -16,6 +20,23 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        root.signInButton.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_homeFragment_to_loginFragment)
+        }
+        root.signUpButton.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_homeFragment_to_registerFragment)
+        }
+        if (FirebaseAuth.getInstance().currentUser?.uid != null)
+        {
+            view?.let {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_homeFragment_to_mainFragment)
+            }
+        }
+        return root
     }
 }
